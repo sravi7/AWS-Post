5b6224afd7f1ee111d27ca1644a67f78636140e0
sravi7 <sravi7@users.noreply.github.com>
2014-11-10 16:27:43 -0500

    Update main.php

--- main.php
+++ main.php
@@ -12,11 +12,11 @@ $link=null;
 if(array_key_exists('Submit1',$_POST))
 {
 	// old $_SESSION['engine_mac'] = $_POST['mac']; 
-	#The next line of statement will remove the \r\n at the very end of the MAC address of the engine so that there is no space in the link.
+	# The next line of statement will remove the \r\n at the very end of the MAC address of the engine so that there is no space in the link.
 	# Example: http://cdn.sealykelvin.com/id/0ABCDE /status.json  will be the link instead of http://cdn.sealykelvin.com/id/0ABCDE/status.json if the next line is not performed.
-	$_SESSION['engine_mac']= preg_replace("/[\r\n]*/","",$_POST['mac']);
+	$_SESSION['engine_mac']= strtoupper(preg_replace("/[\r\n]*/","",$_POST['mac']));
 	
-	echo '<p style="font-size:30px;">Please select the JSON file for the engine with MAC Address '.$_POST['mac'].'</p>';
+	echo '<p style="font-size:30px;">Please select the JSON file for the engine with MAC Address '.$_SESSION['engine_mac'].'</p>';
 	display_json_files();
 }
 elseif(array_key_exists('Submit3', $_POST))
@@ -46,7 +46,7 @@ elseif(array_key_exists('Submit3', $_POST))
 elseif(array_key_exists('Submit4', $_POST))
 {
 	$data_string = $_POST['json_content'];
-	$result = file_get_contents('http://sealykelvin.com/endpoint/?id=E1DDEE', null, stream_context_create(array(
+	$result = file_get_contents('http://sealykelvin.com/endpoint/?id='.$_SESSION['engine_mac'], null, stream_context_create(array(
 								'http' => array(
 												'method' => 'POST',
 												'header' => 'Content-Type: application/json' . "\r\n"
