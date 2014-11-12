1d5bba461391587006967cde2a98dd4dd23ef8a1
sravi7 <sravi7@users.noreply.github.com>
2014-11-12 14:40:45 -0500

    Change to "Submit4" and Deleting an engine
    1. This update gives two options to the user after submitting the data to cloud. 1. To work with the same engine. 2. To work with a different engine. Depending upon the user action, the respective action will take place.
    2. Created an option to delete the engine from the "engine.txt" file if the user decides that the engine is not required.

--- main.php
+++ main.php
@@ -3,14 +3,23 @@
 # Author: Santosh Ravi
 # Created on 10/27/2014
 # Update on 11.11.2014: Changed the "Submit2" case where the if condition will be looking for "engine_mac", which was previously "mac".
+#
 # Update on 11.11.2014: Changed the "Submit2" case where instead of just submitting the engine's MAC address, the MAC address is concatenated with a new line character so that 
-#			testing can be performed with 6 or more characters.
+#						testing can be performed with 6 or more characters.
+#
+# Update on 11.12.2014: 1. Changed the "Submit4" case, where after submitting the data to the cloud, the user is given two options,i.e., 
+#				(a) To work with the same engine, 
+#				(b) To work with a different engine.
+#			Depending upon the user action, the respective actions would take place.
+#			2. Created an option to delete the engine from the "engine.txt" file if the user decides that the engine is not required.
+
 include 'header.php';
 include 'functions.php';
 
 session_start();
 $_SESSION['link']="http://cdn.sealykelvin.com/id/";
 $link=null;
+
 if(array_key_exists('Submit1',$_POST))
 {
 	// old $_SESSION['engine_mac'] = $_POST['mac']; 
@@ -21,6 +30,7 @@ if(array_key_exists('Submit1',$_POST))
 	echo '<p style="font-size:30px;">Please select the JSON file for the engine with MAC Address '.$_SESSION['engine_mac'].'</p>';
 	display_json_files();
 }
+
 elseif(array_key_exists('Submit3', $_POST))
 {
 	if(array_key_exists('json', $_POST))
@@ -28,11 +38,11 @@ elseif(array_key_exists('Submit3', $_POST))
 		$_SESSION['json_file_name']=$_POST['json'];
 		$_SESSION['link']=$_SESSION['link'].$_SESSION['engine_mac']."/".$_POST['json'];
 		// echo "<h1>".$_SESSION['link']."</h1>";
-		display_json_files();
 		// $link=file_get_contents($_SESSION['link']);
 		$_SESSION['json_data']=file_get_contents($_SESSION['link']);
 		// $_SESSION['json_data']=$link;
 		// $link=$link.$_SESSION['engine_mac'];	
+		display_json_files();
 		echo '<hr>';
 		echo "<p style=\"font-size:30px;\">This is the <strong>".$_POST['json']."</strong> file for the engine with MAC address <strong>".$_SESSION['engine_mac']."</strong>.</p>";
 		echo '<form id="editor" method="post" action="'.$_SERVER['PHP_SELF'].'">
@@ -46,6 +56,7 @@ elseif(array_key_exists('Submit3', $_POST))
 		display_json_files();
 	}
 }
+
 elseif(array_key_exists('Submit4', $_POST))
 {
 	$data_string = $_POST['json_content'];
@@ -59,7 +70,15 @@ elseif(array_key_exists('Submit4', $_POST))
 	)));
 	echo '<p style="font-size:25px;"> Data has been submitted to change the contents of <strong>'.$_SESSION['json_file_name'].'</strong> file for the engine with MAC address <strong>'.$_SESSION['engine_mac'].'</strong>.</p>';
 	display_json_files();
+	echo '<form method="post" action="http://localhost/Kelvin_Engine_Wi-Fi/Testing_Code/main.php"><input type="Submit" name="start_beginning" value="Start from the First!!!!" /></form>';
 }
+
+elseif(array_key_exists('start_beginning', $_POST))
+{
+	session_destroy();
+	display_engine();
+}
+
 elseif(array_key_exists('Submit2', $_POST))
 {
 	if(array_key_exists('engine_mac',$_POST))
@@ -76,13 +95,21 @@ elseif(array_key_exists('Submit2', $_POST))
 		display_json_files();
 	}
 }
+
 elseif(array_key_exists('new_engine',$_POST))
 {
 	insert_new_engine();
 }
-else
+
+elseif(array_key_exists('delete_engine', $_POST))
 {
+	$file_contents=file_get_contents("C:\\Apache24\\htdocs\\Kelvin_Engine_Wi-Fi\\engine.txt");
+	$file_contents=str_replace($_POST['mac'],'', $file_contents);
+	file_put_contents("C:\\Apache24\\htdocs\\Kelvin_Engine_Wi-Fi\\engine.txt", $file_contents);
 	display_engine();
 }
 
-echo '</div></body></html>'; 
+ else
+{
+	display_engine();
+}
