cffba7100203dcbdf266e8ccac13946d89902906
sravi7 <sravi7@users.noreply.github.com>
2014-11-12 15:18:31 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,16 +3,16 @@
 # Author: Santosh Ravi
 # Created on 10/27/2014
 # Update on 11.11.2014: In order to test for engine names, instead of restricting the number of characters to display to 6, the program performs a count on the number of elements 
-#			in the array and then will display all the elements except the last element. This is because when you add a new engine in the text file, a new line character 
-#			concatenated at the end. So while displaying the contents of the file, the new line is also displayed, which shouldn't happen. To avoid that, the above mentioned 
-#			operation is performed.
+#						in the array and then will display all the elements except the last element. This is because when you add a new engine in the text file, a new line character 
+#						concatenated at the end. So while displaying the contents of the file, the new line is also displayed, which shouldn't happen. To avoid that, the above mentioned 
+#						operation is performed.
+
 function display_engine()
 {
 	if(file_exists("C:\\Apache24\\htdocs\\Kelvin_Engine_Wi-Fi\\engine.txt"))
 	{
 		$file_contents=file_get_contents("C:\\Apache24\\htdocs\\Kelvin_Engine_Wi-Fi\\engine.txt");
 	}
-	
 	if(!empty($file_contents))
 	{
 		// $contents=str_split($file_contents, 6);
@@ -20,7 +20,6 @@ function display_engine()
 		$contents=explode("\n",$file_contents);
 		$count=count($contents);
 		$contents = array_splice($contents, 0, $count-1);
-		// print_r($contents);
 		echo '<p style="font-size:30px;">Please select the engine you would like to work.</p>
 				<form id="select_mac" method="post" action="'.$_SERVER['PHP_SELF'].'" style="font-size:20px;">
 					<label>MAC Address of the engine</label><br/>';
@@ -29,21 +28,24 @@ function display_engine()
 			echo '<input type="radio" name="mac" value="'.$key.'" id="get_mac_input'.$id.'"/><label for="get_mac_input'.$id.'">'.strtoupper($key).'</label><br/>';
 		}
 		echo '<input type="submit" name="Submit1" value="Submit"/>
-				<input type="submit" name="new_engine" value="New Engine"/></form><br/>';			
+				<input type="submit" name="new_engine" value="New Engine"/>
+				<input type="submit" name="delete_engine" value="Delete Engine"/></form><br/>';			
 	}
 	else
 	{
 		insert_new_engine();
 	}
 }
+
 function insert_new_engine()
 {
-	echo '<p>Please enter the MAC Address for the engine you would like to work.</p>
+	echo '<p style="font-size:20px;">Please enter the MAC Address for the engine you would like to work.</p>
 			<form id="get_mac" method="post" action="'.$_SERVER['PHP_SELF'].'">
 				<label>MAC Address of the engine</label><input type="text" id="get_mac_input" name="engine_mac"/>
 				<input type="submit" name="Submit2" value="Submit"/>
 			</form>';			
 }
+
 function display_json_files()
 {
 	echo '<p style="font-size:20px;font-weight:bold">Select an option to proceed.</p>
