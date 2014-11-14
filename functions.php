6a23020fa7b64510233d499e33dcf5766b53d493
sravi7 <sravi7@users.noreply.github.com>
2014-11-14 16:30:30 -0500

    Added two new functions
    Added the change_sequence_number() and the update_sequence_number() to perform an update on the sequence number in the JSON file.

--- functions.php
+++ functions.php
@@ -6,6 +6,11 @@
 #						in the array and then will display all the elements except the last element. This is because when you add a new engine in the text file, a new line character 
 #						concatenated at the end. So while displaying the contents of the file, the new line is also displayed, which shouldn't happen. To avoid that, the above mentioned 
 #						operation is performed.
+# Update on 11.14.2014: 1. Added the "start_beginning" form action inside the display_json_files() functions.
+#			2. Added 2 new form actions. Their usage are as below:
+#				(a) The "change_sequence_number()" function is the form where the user is given the option to enter the sequence number or directly increment by 1.
+#				(b) The "update_sequence_number()" function is to update the sequence number in the JSON file. The function updates the sequence number and
+#				then displays the data to the user before submitting it to the cloud.
 
 function display_engine()
 {
@@ -58,4 +63,43 @@ function display_json_files()
 			<input type="radio" name="json" value="sleep-b.json" id="json6"/><label for="json6">Sleep-b.json</label><br/>
 			<input type="submit" name="Submit3" value="Submit"/>
 		</form><br/>';
+	echo '<form method="post" action="'.$_SERVER['PHP_SELF'].'"><input type="Submit" name="start_beginning" value="Start from the First!!!!" /></form><br/><hr>';
+}
+
+function display_json_data()
+{
+	echo '	<div id="json_editor"><div id="json_form_left"><p style=\"font-size:30px;\">This is the <strong>"'.$_SESSION['json_file_name'].'"</strong> file for the engine with MAC address <strong>'.$_SESSION['engine_mac'].'</strong>.</p><br/>';
+	change_sequence_number();
+	echo '<form id="editor" method="post" action="'.$_SERVER['PHP_SELF'].'">
+			<textarea id="file_contents" rows=20% cols=90% name="json_content">'.$_SESSION['json_data'].'</textarea><br/>		
+			<input type="submit" value="Submit" name="Submit4"/></form><br/></div>';
+}
+
+function change_sequence_number()
+{
+	$session_json_data=json_decode($_SESSION['json_data']);
+	// var_dump($session_json_data);
+	/* $contents=array();
+	foreach($session_json_data as $name=>$value)
+	{
+		$contents[$name]=$value;
+	}
+	
+	print_r($contents); */
+	// var_dump($contents['settings']['header']['sequence']);
+	echo '<form id="editor" method="post" action="'.$_SERVER['PHP_SELF'].'">
+			<label>Change the sequence number here</label><input type="text" name="sequence_number" value="'.$session_json_data->settings->header->sequence.'"/>
+			<input type="submit" value="Submit" name="Submit_Sequence_Number"/>
+			<input type="submit" value="Increment by 1" name="Submit_Increment_1"/>
+			</form><br/>';
+	// echo $session_json_data->settings->header->sequence;
+}
+
+function update_sequence_number()
+{
+	$session_json_data=json_decode($_SESSION['json_data']);
+	$session_json_data->settings->header->sequence=((array_key_exists('Submit_Sequence_Number',$_POST))?$_POST['sequence_number']:($_POST['sequence_number']+1));
+	$_SESSION['json_data']=json_encode($session_json_data);
+	display_json_files();
+	display_json_data();
 }
